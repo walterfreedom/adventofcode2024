e5818eb1a7d138dda381c9eb2b18826b54048486
walterfreedom <lookslikecat@gmail.com>
2024-12-03 01:13:49 +0300

    gettin there (part 2)

--- day2/day2.go
+++ day2/day2.go
@@ -9,9 +9,11 @@ import (
 )
 
 var total int
+var safe bool = true
+var oldnumbew *int
 
 func main() {
-	file, err := os.Open("testinput")
+	file, err := os.Open("input")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,31 +21,104 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		var intList []int
 		line := scanner.Text()
 		numbewsString := strings.Fields(line)
-		println(line)
-		var oldnumbew *int
-		safe := true
-		for j := range numbewsString {
 
+		safe = true
+
+		//conv list string to list int
+		for j := range numbewsString {
 			intNumbew, err := strconv.Atoi(numbewsString[j])
 			if err != nil {
 				println("oh no")
 			}
-			//I dont really mind the overhead :3
-			if oldnumbew == nil {
-				oldnumbew = &intNumbew
-
-			} else if -3 > (*oldnumbew-intNumbew) || (*oldnumbew-intNumbew) > 3 || (*oldnumbew-intNumbew) == 0 {
-				safe = false
-				print("this bad")
-				break
-			}
-			oldnumbew = &intNumbew
+			intList = append(intList, intNumbew)
 		}
-		if safe {
-			total += 1
+
+		part2(intList)
+	}
+
+	print(total)
+}
+
+func part1(intList []int) {
+	oldnumbew = &intList[0]
+	isInc := false
+	if intList[0] < intList[1] {
+		isInc = true
+	} else if intList[0] == intList[1] {
+		return
+	}
+
+	for j := range intList {
+		newnumbew := intList[j]
+		if j > 0 {
+			if isInc {
+				if -3 > (*oldnumbew-newnumbew) || newnumbew <= *oldnumbew {
+					safe = false
+					break
+				}
+			} else {
+				if (*oldnumbew-newnumbew) > 3 || newnumbew >= *oldnumbew {
+					safe = false
+					break
+				}
+			}
+		}
+		oldnumbew = &newnumbew
+	}
+	if safe {
+		total += 1
+	}
+}
+
+func part2(intList []int) {
+	oldnumbew = &intList[0]
+	safeOnce := true
+	isInc := false
+	startfrom := 0
+	if intList[0] < intList[1] {
+		isInc = true
+	} else if intList[0] == intList[1] {
+		safeOnce = false
+		startfrom = 1
+		intList[1] = intList[0]
+		if intList[0] < intList[2] {
+			isInc = true
+		} else if intList[0] == intList[2] {
+			return
 		}
 	}
-	print(total)
+
+	for j := range intList {
+		newnumbew := intList[j]
+		if j > startfrom {
+			if isInc {
+				if -3 > (*oldnumbew-newnumbew) || newnumbew <= *oldnumbew {
+					if !safeOnce {
+						safe = false
+						break
+					} else {
+						safeOnce = false
+						continue
+					}
+				}
+			} else {
+				if (*oldnumbew-newnumbew) > 3 || newnumbew >= *oldnumbew {
+					if !safeOnce {
+						safe = false
+						break
+					} else {
+						safeOnce = false
+						continue
+					}
+				}
+			}
+		}
+		oldnumbew = &newnumbew
+	}
+	if safe {
+		total += 1
+	}
 }
